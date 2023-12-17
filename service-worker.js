2cf29c2e088a4293dfe3d5aa14c816c4b1d485b3
Baris Senkal <barissenkal@gmail.com>
2023-12-17 15:02:17 +0300

    Updated service worker base path for github pages

--- service-worker.js
+++ service-worker.js
@@ -8,31 +8,47 @@
 const sw = self;
 
 const CACHE_NAME = "main-cache-20190216";
+const BASE_PATH = "/DrinkingGame"; // Github pages path for hosting
 const CACHE_PATHS = [
 	// Main single file page
 	"/",
 	// Just in case
 	"/index.html",
 	// Icons etc
-	"/icons/apple-touch-icon-57x57.png",
-	"/icons/apple-touch-icon-60x60.png",
-	"/icons/apple-touch-icon-72x72.png",
-	"/icons/apple-touch-icon-76x76.png",
+	"/icons/android-chrome-144x144.png",
+	"/icons/android-chrome-192x192.png",
+	"/icons/android-chrome-36x36.png",
+	"/icons/android-chrome-48x48.png",
+	"/icons/android-chrome-72x72.png",
+	"/icons/android-chrome-96x96.png",
 	"/icons/apple-touch-icon-114x114.png",
 	"/icons/apple-touch-icon-120x120.png",
 	"/icons/apple-touch-icon-144x144.png",
 	"/icons/apple-touch-icon-152x152.png",
 	"/icons/apple-touch-icon-180x180.png",
-	"/icons/favicon-32x32.png",
-	"/icons/android-chrome-192x192.png",
-	"/icons/favicon-96x96.png",
-	"/icons/favicon-16x16.png",
-	"/icons/manifest.json",
-	"/icons/safari-pinned-tab.svg",
-	"/icons/favicon.ico",
-	"/icons/mstile-144x144.png",
+	"/icons/apple-touch-icon-57x57.png",
+	"/icons/apple-touch-icon-60x60.png",
+	"/icons/apple-touch-icon-72x72.png",
+	"/icons/apple-touch-icon-76x76.png",
+	"/icons/apple-touch-icon-precomposed.png",
+	"/icons/apple-touch-icon.png",
 	"/icons/browserconfig.xml",
-]
+	"/icons/favicon-16x16.png",
+	"/icons/favicon-32x32.png",
+	"/icons/favicon-96x96.png",
+	"/icons/favicon.ico",
+	"/icons/manifest.json",
+	"/icons/mstile-144x144.png",
+	"/icons/mstile-150x150.png",
+	"/icons/mstile-310x150.png",
+	"/icons/mstile-310x310.png",
+	"/icons/mstile-70x70.png",
+	"/icons/safari-pinned-tab.svg",
+	"/icons/startup_image_iPhone4s.png",
+	"/icons/startup_image_iPhone5.png",
+	"/icons/startup_image_iPhone6.png",
+	"/icons/startup_image_iPhone6Plus.png"
+].map((path) => BASE_PATH + path)
 
 sw.addEventListener("install", (event) => {
 	console.debug('[service-worker.js] install')
@@ -67,7 +83,7 @@ sw.addEventListener("fetch", (event) => {
 
 		return response;
 	}, (error) => {
-		console.error('[service-worker.js] fetch error', error)
+		console.debug('[service-worker.js] fetch error', error)
 		return cachePromise.then((cache) => {
 			return cache.match(event.request).then((response) => {
 				if (response) {
